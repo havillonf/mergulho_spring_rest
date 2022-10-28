1b9eea3aa170c1430862a2704574b23ab83401e8
havillonf <havillonbfreitas@gmail.com>
2022-10-28 17:48:33 -0300

    Criação do Model Categoria

--- aula_01_stream/model/Categoria.java
+++ aula_01_stream/model/Categoria.java
@@ -1,5 +1,13 @@
 package aula_01_stream.model;
 
 public class Categoria {
-    
+    String nomeCategoria;
+
+    public Categoria (String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+
+    public String getCategoria () {
+        return this.nomeCategoria;
+    }
 }
