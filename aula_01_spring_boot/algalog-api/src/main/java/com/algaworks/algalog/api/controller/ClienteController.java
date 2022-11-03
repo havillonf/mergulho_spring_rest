6f3bbdf02730b90021f89bf034446512796dbaa4
havillonf <havillonbfreitas@gmail.com>
2022-11-03 17:42:18 -0300

    Configuracoes do mysql e ClienteRepository

--- aula_01_spring_boot/algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ aula_01_spring_boot/algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,31 +1,28 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
+	@PersistenceContext
+	private EntityManager manager;
+
+	private ClienteRepository clienteRepository;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("85 99122-1232");
-		cliente1.setEmail("joao@email.com");
-		
-		Cliente cliente2 = new Cliente();
-		
-		cliente2.setId(1L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("85 98272-1232");
-		cliente2.setEmail("maria@email.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll();		
 	}
 }
