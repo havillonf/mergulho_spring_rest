edcfe0146a1bafac34e98fe619167224c625c04a
havillonf <havillonbfreitas@gmail.com>
2022-11-07 14:43:21 -0300

    Implementacao da criacao de um cliente

--- aula_01_spring_boot/algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ aula_01_spring_boot/algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
@@ -18,7 +22,7 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping
+@RequestMapping("/clientes")
 public class ClienteController {
 	@PersistenceContext
 	private EntityManager manager;
@@ -27,7 +31,7 @@ public class ClienteController {
 
 	@GetMapping
 	public List<Cliente> listar() {
-		return clienteRepository.findByNome("João");		
+		return clienteRepository.findAll();		
 	}
 
 	@GetMapping("/{clienteId}")
@@ -44,5 +48,11 @@ public class ClienteController {
 		// }
 
 		// return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente){
+		return clienteRepository.save(cliente);
 	}
 }
